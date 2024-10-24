c36a6b5b5a2461d780904ebd7a63ac06e549300b
Alan <xiefengan013@gmail.com>
2024-10-24 22:11:38 +0800

    [Modified] Revise person_main.py for personal usage 1. Added EPSLists for use 2. Revise args for use medium EPS from crwal datas

--- person_main.py
+++ person_main.py
@@ -22,7 +22,7 @@ Description: select forward eps value
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-sel",   type=int, default=1, help='Select EPS year, N: This year, 0: N+0, 1: N+1, 2: N+2')
-parser.add_argument("-level", type=int, default=3, help='Select forward eps value\n0: high, 1: low, 2: average, 3: medium')
+parser.add_argument("-level", type=int, default=4, help='Select forward eps value\n1: high, 2: low, 3: average, 4: medium')
 parser.add_argument("-year",  type=float, default=4.5, help="Data calculation length(unit:year)")
 parser.add_argument("-e_eps", type=float, default=None)
 
@@ -38,7 +38,7 @@ from utils.utils import ModifideParameter, Parameter_read
 
 if __name__ == "__main__":
     new_result = Path("results")
-
+    EPSLists = [] # set your EST eps in here
     with open("token.yaml", "r") as file:
         Token = yaml.safe_load(file)
 
@@ -84,11 +84,10 @@ if __name__ == "__main__":
             print("Enter Error!!")
             input()
             continue
-
         for title, StockList in StockLists.items():
             print(title, StockList)
 
             # Get Data
-            calculator(Database, StockList, parameter, new_result / Path(title))
+            calculator(Database, StockList, EPSLists, parameter, new_result / Path(title))
         print("Enter to continue...")
         input()

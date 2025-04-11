52dea65f07b772bccd02b3d3488aa51d2517b69a
Alan <xiefengan013@gmail.com>
2025-04-11 22:37:10 +0800

    [Fix] Reivse summary table from Yahoo finance

--- Database/YahooFinance.py
+++ Database/YahooFinance.py
@@ -1,27 +1,32 @@
 from utils.utils import fetch_webpage
 
-
+# Get the Yahoo Finance summary for the stock
+# This function is support Taiwan stock market only
 class YahooFinance:
     def __init__(self, stock_id, Market):
+        # Check if the Market is valid
+        if Market != "TW" and Market != "TWO":
+            print(f"Market: {Market} is not supported, please use TW or TWO")
+
         self.stock_id = stock_id
         self.Market = Market
         self.summary = self.get_yahooFinanceDict()
-
+        
     def get_yahooFinanceDict(self):
         yahooFinanceDict = {}
         url = f"https://finance.yahoo.com/quote/{self.stock_id}.{self.Market}/"
         soup = fetch_webpage(url)
+        
+        # scrape the page for the desired data
         for item in soup.find("div", {"data-testid": "quote-statistics"}).select("li"):
-            label = item.find("span", class_="label yf-dudngy").text
-            value_span = item.find("span", class_="value yf-dudngy")
-            fin_streamer = value_span.find("fin-streamer")
+            labels = item.find_all("span")
+            if len(labels) < 2:
+                continue
 
-            if fin_streamer:
-                value = fin_streamer["data-value"]
-            else:
-                value = value_span.text.strip()
-
-            yahooFinanceDict[label] = value
+            # Get the key and value from the labels
+            key = labels[0].text.strip()
+            value = labels[1].text.strip()
+            yahooFinanceDict[key] = value
 
         return yahooFinanceDict
 
@@ -29,6 +34,6 @@ class YahooFinance:
         data = None
         try:
             data = float(self.summary["1y Target Est"].replace(",", ""))
-        except:
-            pass
+        except Exception as e:
+            print(f"Error: {e}")
         return data
